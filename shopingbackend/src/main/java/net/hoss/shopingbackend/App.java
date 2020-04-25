59dca5f90a241a5f6711c0de10e887cbd9d5c25b
hossameldin94 <hossameldin.youssef94@gmail.com>
2020-04-25 02:02:48 +0200

    first initial commit

---
+++ shopingbackend/src/main/java/net/hoss/shopingbackend/App.java
@@ -0,0 +1,13 @@
+package net.hoss.shopingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
